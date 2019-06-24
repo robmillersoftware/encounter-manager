b31d46a123a206b4fea7a04ffe69ddd506017638
Robert Miller <robert.miller@curavihealth.com>
2019-06-24 13:21:00 -0400

    REBOOTING

--- scripts/npm/build.js
+++ scripts/npm/build.js
@@ -3,9 +3,9 @@ const { execSync } = require("child_process");
 console.log("----------BUILD STARTED-----------");
 execSync("ionic cordova build android", {stdio: 'inherit'});
 console.log("----------BUILD FINISHED----------");
-console.log(`Creating distribution directory at: ${process.env.NODE_PATH}/dist`);
-execSync(`mkdir -p ${process.env.NODE_PATH}/dist`, {stdio: 'inherit'});
+console.log(`Creating distribution directory at: ./dist`);
+execSync(`mkdir -p ./dist`, {stdio: 'inherit'});
 console.log("------CREATED DIST DIRECTORY------");
 console.log("Copying APK to distribution directory.");
-execSync(`cp ${process.env.NODE_PATH}/platforms/android/app/build/outputs/apk/debug/app-debug.apk ${process.env.NODE_PATH}/dist/retcon.apk`, {stdio: 'inherit'});
+execSync(`cp ./platforms/android/app/build/outputs/apk/debug/app-debug.apk ./dist/retcon.apk`, {stdio: 'inherit'});
 console.log("---------COPIED APK FILE----------")
